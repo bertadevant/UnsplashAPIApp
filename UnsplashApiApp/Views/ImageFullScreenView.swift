b02cec2f6ad91ab86619f1105e8ac37f966ad4fc
Berta Devant <bertadeva@gmail.com>
2019-08-13 16:51:40 +0200

    create a full image class for presenting image selected

---
+++ UnsplashApiApp/Views/ImageFullScreenView.swift
@@ -0,0 +1,9 @@
+//
+//  ImageFullScreenView.swift
+//  UnsplashApiApp
+//
+//  Created by Berta Devant on 12/08/2019.
+//  Copyright © 2019 Berta Devant. All rights reserved.
+//
+
+import Foundation
