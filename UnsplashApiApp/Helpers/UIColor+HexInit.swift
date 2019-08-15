1639d6f404386a748696713606fa6f78bea3552f
Berta Devant <bertadeva@gmail.com>
2019-08-15 23:28:38 +0200

    add a isLight functions to get style for images

--- UnsplashApiApp/Helpers/UIColor+HexInit.swift
+++ UnsplashApiApp/Helpers/UIColor+HexInit.swift
@@ -18,4 +18,24 @@ extension UIColor {
             return nil
         }
     }
+    
+    // Check if the color is light or dark, as defined by the injected lightness threshold.
+    // Some people report that 0.7 is best.
+    // A false value is returned if the lightness couldn't be determined.
+    func isLight(threshold: Float = 0.5) -> Bool {
+        let originalCGColor = self.cgColor
+        
+        // Now we need to convert it to the RGB colorspace. UIColor.white / UIColor.black are greyscale and not RGB.
+        // If you don't do this then you will crash when accessing components index 2 below when evaluating greyscale colors.
+        let RGBCGColor = originalCGColor.converted(to: CGColorSpaceCreateDeviceRGB(), intent: .defaultIntent, options: nil)
+        guard let components = RGBCGColor?.components else {
+            return false
+        }
+        guard components.count >= 3 else {
+            return false
+        }
+        
+        let brightness = Float(((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000)
+        return (brightness > threshold)
+    }
 }
