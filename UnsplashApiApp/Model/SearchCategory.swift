b4b34985edf01b1bba2eb090257c3814da231b01
Berta Devant <bertadeva@gmail.com>
2019-08-20 01:05:02 +0200

    create search bar view for categories

---
+++ UnsplashApiApp/Model/SearchCategory.swift
@@ -0,0 +1,17 @@
+//
+//  SearchCategory.swift
+//  UnsplashApiApp
+//
+//  Created by Berta Devant on 20/08/2019.
+//  Copyright © 2019 Berta Devant. All rights reserved.
+//
+
+import Foundation
+
+struct SearchCategory {
+    let name: String
+}
+
+extension SearchCategory {
+    static var barcelona = SearchCategory(name: "Barcelona")
+}
---
+++ UnsplashApiApp/Views/SearchBarView.swift
@@ -0,0 +1,79 @@
+//
+//  SearchBarView.swift
+//  UnsplashApiApp
+//
+//  Created by Berta Devant on 20/08/2019.
+//  Copyright © 2019 Berta Devant. All rights reserved.
+//
+
+import UIKit
+
+protocol SearchDelegate: class {
+    func searchQuery(_ query: String)
+}
+
+class SearchBarView: UIView {
+    weak var delegate: SearchDelegate?
+    
+    private var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.barStyle = UIBarStyle.default
+        bar.barTintColor = .white
+        return bar
+    }()
+    
+    private var stackView: UIStackView = {
+        let view = UIStackView()
+        view.distribution = .fillEqually
+        view.axis = .horizontal
+        view.spacing = 8
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("This view is not designed to be used with xib or storyboard files")
+    }
+    
+    func setSearchBar(with categories: [SearchCategory]) {
+        for category in categories {
+            let button = UIButton()
+            button.setTitle(category.name, for: .normal)
+            button.tintColor = .gray
+            button.addTarget(self, action: #selector(categoryButtonTapped), for: .touchUpInside)
+            stackView.addArrangedSubview(button)
+        }
+    }
+    
+    @objc func categoryButtonTapped(_ sender: UIButton) {
+        guard let searchText = sender.currentTitle else {
+            return
+        }
+        delegate?.searchQuery(searchText)
+    }
+    
+    private func setup() {
+        addSubview(searchBar)
+        addSubview(stackView)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        searchBar.pinToSuperview(edges: [.top, .left, .right])
+        stackView.pinToSuperview(edges: [ .bottom, .left, .right])
+        stackView.pin(edge: .top, to: .bottom, of: searchBar, constant: 8)
+    }
+}
+
+extension SearchBarView: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text else {
+            return
+        }
+        delegate?.searchQuery(searchText)
+    }
+}
