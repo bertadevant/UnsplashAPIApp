5991ec7ac10c3f993b23d5c52be87a6f3f2937d8
Olivier Halligon <olivier.halligon@babylonhealth.com>
2020-04-03 23:21:05 +0200

    Adding an indicator to UIImageView while loading
    Though this doesn't solve the problem that it shouldn't be the responsibility of the View layer to load an image and that it's gonna lead to cell recycling bugs

--- UnsplashApiApp/Helpers/UIImageView+AsyncDownload.swift
+++ UnsplashApiApp/Helpers/UIImageView+AsyncDownload.swift
@@ -14,12 +14,36 @@ extension UIImageView {
         self.image = nil
         let request = LoadAPIRequest(imageURL: imageURL)
         let resource = Resource<Data>(get: request)
+        let indicator = addLoadingIndicator()
         Dependencies.enviroment.session.download(resource) { imageData, _ in
             let image = imageData.map(UIImage.init(data:))
             DispatchQueue.main.async {
                 self.image = image ?? placeHolder
+                indicator.removeFromSuperview()
             }
         }
+    }
+
+    private func addLoadingIndicator() -> UIView {
+        let roundedSquare = UIView()
+        roundedSquare.backgroundColor = Colors.darkGray.withAlphaComponent(0.3)
+        roundedSquare.layer.cornerRadius = 20.0
+        roundedSquare.layer.masksToBounds = true
+        self.addSubview(roundedSquare)
+        roundedSquare.addWidthConstraint(with: 80)
+        roundedSquare.addHeightConstraint(with: 80)
+        roundedSquare.centerXToSuperview()
+        roundedSquare.centerYToSuperview()
+
+        let indicator = UIActivityIndicatorView(style: .whiteLarge)
+        roundedSquare.addSubview(indicator)
+        indicator.sizeToFit()
+        indicator.centerXToSuperview()
+        indicator.centerYToSuperview()
+
+        indicator.startAnimating()
+
+        return roundedSquare
     }
 }
 
