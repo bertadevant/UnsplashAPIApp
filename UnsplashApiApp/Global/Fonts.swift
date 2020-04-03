2b7c4f93e1886a67f16c929817e46b1997fa5653
Olivier Halligon <olivier.halligon@babylonhealth.com>
2020-04-03 21:51:04 +0200

    Move declaration of Fonts constants to dedicated file

---
+++ UnsplashApiApp/Global/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  UnsplashApiApp
+//
+//  Created by Olivier Halligon on 03/04/2020.
+//  Copyright © 2020 Berta Devant. All rights reserved.
+//
+
+import UIKit
+
+struct Fonts {
+    static var regular = UIFont(name: "HelveticaNeue", size: 16) ?? UIFont.systemFont(ofSize: 16)
+}
