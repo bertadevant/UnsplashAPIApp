c218c999e33902294b51ebca69516db9dbdc42c8
Berta Devant <bertadeva@gmail.com>
2020-04-06 00:51:05 +0200

    clean up the hex code

--- UnsplashApiApp/Helpers/UIColor+HexInit.swift
+++ UnsplashApiApp/Helpers/UIColor+HexInit.swift
@@ -7,35 +7,23 @@ extension UIColor {
             .replacingOccurrences(of: "#", with: "")
         
         var rgbValue: UInt32 = 0
-        if Scanner(string: hexString).scanHexInt32(&rgbValue) {
-            self.init(
-                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                alpha: CGFloat(1.0)
-            )
-        } else {
+        guard Scanner(string: hexString).scanHexInt32(&rgbValue) else {
             return nil
         }
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
     
     // Check if the color is light or dark, as defined by the injected lightness threshold.
     // Some people report that 0.7 is best.
     // A false value is returned if the lightness couldn't be determined.
-    func isLight(threshold: Float = 0.5) -> Bool {
-        let originalCGColor = self.cgColor
-        
-        // Now we need to convert it to the RGB colorspace. UIColor.white / UIColor.black are greyscale and not RGB.
-        // If you don't do this then you will crash when accessing components index 2 below when evaluating greyscale colors.
-        let RGBCGColor = originalCGColor.converted(to: CGColorSpaceCreateDeviceRGB(), intent: .defaultIntent, options: nil)
-        guard let components = RGBCGColor?.components else {
-            return false
-        }
-        guard components.count >= 3 else {
-            return false
-        }
-        
-        let brightness = Float(((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000)
+    func isLight(threshold: CGFloat = 0.5) -> Bool {
+        var brightness: CGFloat = 0.0
+        self.getHue(nil, saturation: nil, brightness: &brightness, alpha: nil)
         return (brightness > threshold)
     }
 }
