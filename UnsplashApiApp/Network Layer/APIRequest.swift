d1f37be5926dbb1f28dccb7a548328c666712db0
Berta Devant <bertadeva@gmail.com>
2020-04-06 12:15:09 +0200

    refactor the image requests to use a struct APIRequests instead of different types

--- UnsplashApiApp/Network Layer/APIRequest.swift
+++ UnsplashApiApp/Network Layer/APIRequest.swift
@@ -8,6 +8,36 @@
 
 import Foundation
 
+struct APIRequest {
+    let components: URLComponents
+}
+
+extension APIRequest {
+    static func imageRequest(searchParameters: SearchParameters) -> APIRequest {
+        var queryItems: [URLQueryItem] = []
+        let page: String = searchParameters.page == 0 ? "1" : searchParameters.page.description
+        queryItems.append(URLQueryItem(name: "page", value: page))
+        if !searchParameters.query.isEmpty {
+            queryItems.append(URLQueryItem(name: "query", value: searchParameters.query))
+        }
+        let components = unsplashComponent(path: searchParameters.searchType.rawValue, queryItems: queryItems)
+        return APIRequest(components: components)
+    }
+    
+    //Unsplash API demands that we trigger a download count when downloading a picture, this request is created to handle that
+    // https://unsplash.com/documentation#track-a-photo-download
+    static func downloadRequest(imageID: String) -> APIRequest {
+        let components = unsplashComponent(path: "/photos/\(imageID)/download")
+        return APIRequest(components: components)
+    }
+    
+    //Each Image parsed from the API comes with the urls for downloading the images, we use this request to get the proper URL for downloading for each image
+    static func loadRequest(imageURL: String) -> APIRequest {
+        let components = URLComponents(string: imageURL) ?? URLComponents()
+        return APIRequest(components: components)
+    }
+}
+
 private func unsplashComponent(path: String, queryItems: [URLQueryItem] = []) -> URLComponents {
     var component = URLComponents()
     component.scheme = "https"
@@ -15,45 +45,4 @@ private func unsplashComponent(path: String, queryItems: [URLQueryItem] = []) ->
     component.path = path
     component.queryItems = queryItems
     return component
-}
-
-protocol APIRequest {
-    var components: URLComponents { get }
-}
-
-final class ImageAPIRequest: APIRequest {
-    let searchParameters: SearchParameters
-    
-    var components: URLComponents {
-        var queryItems: [URLQueryItem] = []
-        let page: String = searchParameters.page == 0 ? "1" : searchParameters.page.description
-        queryItems.append(URLQueryItem(name: "page", value: page))
-        if !searchParameters.query.isEmpty {
-            queryItems.append(URLQueryItem(name: "query", value: searchParameters.query))
-        }
-        return unsplashComponent(path: searchParameters.searchType.rawValue, queryItems: queryItems)
-    }
-    
-    init(search: SearchParameters) {
-        self.searchParameters = search
-    }
-}
-
-//Unsplash API demands that we trigger a download count when downloading a picture, this request is created to handle that
-// https://unsplash.com/documentation#track-a-photo-download
-final class DownloadAPIRequest: APIRequest {
-    let components: URLComponents
-    
-    init(imageID: String) {
-        self.components = unsplashComponent(path: "/photos/\(imageID)/download")
-    }
-}
-
-//Each Image parsed from the API comes with the urls for downloading the images, we use this request to get the proper URL for downloading for each image
-final class LoadAPIRequest: APIRequest {
-    let components: URLComponents
-    
-    init(imageURL: String) {
-        self.components = URLComponents(string: imageURL) ?? URLComponents()
-    }
 }
