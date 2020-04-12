ca1b8e874375d9b8b4cae3152ee68a72a8e825c9
Berta Devant <bertadeva@gmail.com>
2020-04-12 20:37:37 +0200

    delete unusable test data and doubles

---
+++ UnsplashApiAppTests/TestData/ImageViewState+TestData.swift
@@ -0,0 +1,9 @@
+//
+//  ImageViewState+TestData.swift
+//  UnsplashApiAppTests
+//
+//  Created by Berta Devant on 12/04/2020.
+//  Copyright © 2020 Berta Devant. All rights reserved.
+//
+
+import Foundation
